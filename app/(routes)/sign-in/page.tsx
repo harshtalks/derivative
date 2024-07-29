64d13ca463b0e9a026a289083bb731f5a3c76e84
Harsh Pareek <harshpareek@Harshs-MacBook-Air.local>
2024-07-29 23:11:05 +0530

    fix: better sign page

--- app/(routes)/sign-in/page.tsx
+++ app/(routes)/sign-in/page.tsx
@@ -6,6 +6,7 @@ import { HomeIcon } from "lucide-react";
 import React from "react";
 import AuthInterceptor from "@/auth/authIntercepter";
 import SignInPage from "./route.info";
+import Header from "@/app/_components/header";
 
 const Page = async ({
   searchParams,
@@ -18,38 +19,56 @@ const Page = async ({
 
   return (
     <section>
-      <div className="px-8 py-48  mx-auto md:px-12 lg:px-32 max-w-7xl">
-        <div className="max-w-md mx-auto md:max-w-sm md:w-96">
-          <div className="flex flex-col text-center">
-            <h1 className="text-3xl font-semibold tracking-tighter ">
-              Collaborating on single-page projects,
-              <span className="text-gray-600">from any location</span>
-            </h1>
-            <p className="mt-4 text-base font-medium ">
-              A rapid approach to collaborate in staging and provisional
-              settings.
-            </p>
-          </div>
-          <div className="mt-8 gap-4 w-full flex flex-col items-center justify-center">
-            <GithubAuth.Link
-              params={{}}
-              searchParams={{
-                redirectUrl: searchParams.redirectUrl?.toString(),
-              }}
-            >
-              <Button
-                className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500"
-                aria-label="Sign in with Google"
+      <div className="relative z-40 h-full w-full bg-background">
+        <div className="dark:hidden -z-10 absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <div className="hidden -z-10 dark:block absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+        <Header />
+        <div className="grid grid-rows-[1fr_auto]">
+          <div className="px-8 py-48 mx-auto md:px-12 lg:px-32 max-w-7xl">
+            <div className="flex flex-col text-center">
+              <h1>
+                <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-5xl lg:text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+                  start building with derivative
+                </span>
+              </h1>
+            </div>
+            <div className="mt-8 gap-4 w-full flex flex-col items-center justify-center">
+              <GithubAuth.Link
+                params={{}}
+                searchParams={{
+                  redirectUrl: searchParams.redirectUrl?.toString(),
+                }}
               >
-                <GitHubLogoIcon className="size-6" />
-                <span>Sign in with Github</span>
-              </Button>
-            </GithubAuth.Link>
-            <HomePageRoute.Link params={{}}>
-              <Button variant="ghost">
-                <HomeIcon />
-              </Button>
-            </HomePageRoute.Link>
+                <Button
+                  aria-label="Sign in with Github"
+                  className="inline-flex gap-2"
+                  variant={"gooeyLeft"}
+                  size="lg"
+                >
+                  <GitHubLogoIcon className="size-5 shrink-0 " />
+                  <span>Sign in with Github</span>
+                </Button>
+              </GithubAuth.Link>
+              <HomePageRoute.Link params={{}}>
+                <Button variant="ghost">
+                  <HomeIcon />
+                </Button>
+              </HomePageRoute.Link>
+            </div>
+          </div>
+          <div className="text-center opacity-50 text-xs max-w-xl mx-auto leading-relaxed px-4">
+            <p>
+              Derivative do not access to your any github repositories and
+              personal content other than your email account, username, full
+              name, and avatar. Derivative allows you to delete your account and
+              all the data associated with it. Exceptions are the workspaces.
+              Workspaces are not deleted when you delete your account. However
+              you can delete the workspaces manually before deleting your
+              account. You can secure your account with passkey powered 2FA.
+              Note that it may be subjected to a change of pricing plans in the
+              future.
+            </p>
           </div>
         </div>
       </div>
